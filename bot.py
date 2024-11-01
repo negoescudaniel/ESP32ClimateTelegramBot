852e26a6687e4b84fd43afbe5f1eb5d2d277aec3
negoescudaniel <128603005+negoescudaniel@users.noreply.github.com>
2024-11-01 17:16:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
 from csvHandling import CSV, plot_temperature_and_humidity_readings
+from connectivity import send_data
+from apscheduler.schedulers.background import BackgroundScheduler
 
 # Set up logging
 logging.basicConfig(
@@ -15,7 +17,11 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"  # Modify as necessary
 
 # Start command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm ESPClimateBot, please talk to me! Use /help to find out my functionalities")
+
+# Help command handler
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="List of commads : \n /start - welcome message \n /help - get informed about commands \n /sensor_data - get real time temperature and humidity from sensor \n /climate_graph - returns a graph with the evolution of temperature and humidity over a given period on time \n /send_photo - returns a possible profile photo for the bot \n /caps - returns the given text with caps \n /echo - returns the given text\n")
 
 # Echo command handler
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,13 +32,13 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-# Print_dates command handler
-async def print_dates(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Extract the full message text after the command '/print_dates'
+# climate_graph command handler
+async def climate_graph(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Extract the full message text after the command '/climateGraph'
     message_text = update.message.text
 
-    # Remove the command part '/print_dates' from the message and strip extra spaces
-    date_args = message_text.replace('/print_dates', '').strip()
+    # Remove the command part '/climate_graph' from the message and strip extra spaces
+    date_args = message_text.replace('/climate_graph', '').strip()
 
     # Split by the '&' character expecting exactly two date-time values
     if '&' not in date_args:
@@ -58,7 +64,6 @@ async def print_dates(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         df = CSV.get_readings(start_date_str, end_date_str)  # This now works correctly
         if df is not None:  # Ensure df is not None before plotting
-            print('SSS')
             plot_path = plot_temperature_and_humidity_readings(df)
         # Send the parsed dates back to the user
         await context.bot.send_message(
@@ -70,8 +75,6 @@ async def print_dates(update: Update, context: ContextTypes.DEFAULT_TYPE):
         with open(plot_path, 'rb') as photo:
             await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo)
 
-        # Optionally, remove the plot image file after sending to save space
-        #os.remove(plot_path)
     except ValueError:
         # If parsing fails, send an error message back to the user
         await context.bot.send_message(
@@ -79,31 +82,73 @@ async def print_dates(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text="Invalid date format. Please use dd-mm-yyyy hh:mm & dd-mm-yyyy hh:mm."
         )
 
+# Test telegram command for sending a photo
 async def send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Define the path to the local image or a URL
-    photo_path = 'Idei.png'  # For a local image
-    # photo_path = 'https://example.com/image.jpg'  # Or use a URL
+    photo_path = 'Idei.png' 
     
     # Send the photo to the user
     await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(photo_path, 'rb'))
 
+# Commands that sends the sensor readings in real time
+async def sensor_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    response = send_data()
+    # Split the response into temperature and humidity
+    try:
+        # Assume response_on is something like "22.33 26.77"
+        temperature, humidity = response.split()
+        
+        # Format the output string
+        formatted_response = f"Temperature: {temperature}°C\nHumidity: {humidity}%"
+    except ValueError:
+        # In case the response does not split correctly
+        formatted_response = "Invalid data format received from ESP32."
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=formatted_response)
+
+
+# Function to fetch data from the sensor at specific times
+def scheduled_sensor_data_fetch():
+    response = send_data()
+    try:
+        CSV.initialize_csv()
+        temperature, humidity = response.split()
+        CSV.add_entry(datetime.now(),temperature = temperature, humidity = humidity+'\n')
+    except ValueError:
+        formatted_response = "Invalid data format received from ESP32."
+    
+  
+
 
 if __name__ == '__main__':
     # Create the application
-    application = ApplicationBuilder().token('7968000892:AAG-fxn8DmR5T4rlLhH6WSQQaSn8ngvM0tA').build()
+    application = ApplicationBuilder().token('YOUR_BOT_TOKEN').build()
     
     # Handlers
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)
-    print_dates_handler = CommandHandler('print_dates', print_dates)  # New print_dates command
+    climate_graph_handler = CommandHandler('climate_graph', climate_graph)  
     send_photo_handler = CommandHandler('send_photo', send_photo)
+    sensor_data_handler = CommandHandler('sensor_data', sensor_data)
     # Add handlers to the application
     application.add_handler(start_handler)
+    application.add_handler(help_handler)
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
     application.add_handler(send_photo_handler)
-    application.add_handler(print_dates_handler)  # Add the new command
+    application.add_handler(climate_graph_handler)  
+    application.add_handler(sensor_data_handler)
+
+    # Set up APScheduler
+    scheduler = BackgroundScheduler()
+
+    # Schedule jobs at specific times (00:00, 04:00, 08:00, 12:00, 16:00, 20:00)
+    scheduler.add_job(scheduled_sensor_data_fetch, 'cron', hour='0,4,8,12,16,20')
+    
+    # Start the scheduler
+    scheduler.start()
 
     # Run the bot
     application.run_polling()
